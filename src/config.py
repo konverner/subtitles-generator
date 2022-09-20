70efb8587da9ca42d6984602676db9402870d12c
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2022-09-20 21:12:33 +0200

    new model name added

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 
-MODEL_NAME = "jonatasgrosman/wav2vec2-large-xlsr-53-german"
+MODEL_NAMES = {'de': "jonatasgrosman/wav2vec2-large-xlsr-53-german",
+               'eng': 'facebook/wav2vec2-base-960h'}
+
 SAMPLING_RATE = 16000
-CHUNK_SIZE = 10  # a size in seconds of one piece of audio used for one subtitles frame
+CHUNK_SIZE = 6  # a size in seconds of one piece of audio used for one subtitles frame
 BATCH_SIZE = 5  # how many chunks are passed to a model at once
+
+SUPPORTED_LANGS = {'eng', 'de'}
+SUPPORTED_FORMATS = {'.wav', '.mp4', '.avi', '.webm'}
