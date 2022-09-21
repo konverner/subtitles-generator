457bf5514812c58479d60e665aa69d1e3b0f27e0
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2022-09-21 23:45:34 +0200

    threshold variable added

--- src/config.py
+++ src/config.py
@@ -1,11 +1,10 @@
-from pathlib import Path
-
 MODEL_NAMES = {'de': "jonatasgrosman/wav2vec2-large-xlsr-53-german",
                'eng': 'facebook/wav2vec2-base-960h'}
 
 SAMPLING_RATE = 16000
 CHUNK_SIZE = 6  # a size in seconds of one piece of audio used for one subtitles frame
 BATCH_SIZE = 5  # how many chunks are passed to a model at once
+LIKELIHOOD_TRESHOLD = 0.5  # threshold value for eliminating phrases that contains out-of-dictionary words
 
 SUPPORTED_LANGS = {'eng', 'de'}
 SUPPORTED_FORMATS = {'.wav', '.mp4', '.avi', '.webm'}
