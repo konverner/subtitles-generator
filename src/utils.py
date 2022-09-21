f53ef6279a330d75bbd7c62d29c977293b8cbe61
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2022-09-21 23:44:50 +0200

    spellchecking tool added

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import datetime
 import pathlib
 
 import imageio
+import spellchecker
 imageio.plugins.ffmpeg.download()
 from moviepy.editor import VideoFileClip
 
@@ -14,22 +15,35 @@ def extract_audio(input_path: pathlib.Path):
     print("extracting audio from video ...")
     video = VideoFileClip(str(input_path))
     audio_path = input_path.with_suffix('.wav')
-    video.audio.write_audiofile(audio_path)
+    video.audio.write_audiofile(audio_path, logger=None)
     return audio_path
 
 
-def create_srt(subtitles_path: pathlib.Path, texts: list, interval_size: int):
+def create_srt(subtitles_path: pathlib.Path, texts: list[str], interval_size: int):
 
     if subtitles_path.suffix != '.srt':
         subtitles_path.rename(subtitles_path.with_suffix('.srt'))
 
     f = open(subtitles_path, 'w')
     timer = 0
-    for i, text in enumerate(texts):
-        start_time = str(datetime.timedelta(seconds=timer))+',000'
-        end_time = str(datetime.timedelta(seconds=timer+interval_size))+',000'
-        frame = f'{i+1}\n{start_time} -->  {end_time}\n{text}\n\n'
-        f.write(frame)
+    frame_counter = 0
+    for text in texts:
+        # if text is None than we need to make a gap in subtitles flow
+        if text is not None:
+            frame_counter += 1
+            start_time = str(datetime.timedelta(seconds=timer))+',000'
+            end_time = str(datetime.timedelta(seconds=timer+interval_size))+',000'
+            frame = f'{frame_counter}\n{start_time} -->  {end_time}\n{text}\n\n'
+            f.write(frame)
         timer += interval_size
-    print(f'{i} subtitle frames are recorded')
+    print(f'{frame_counter} subtitles frames have been generated')
     f.close()
+
+
+def text_likelihood(text: str, dictionary: spellchecker.SpellChecker):
+    words = text.split()
+    known = 0
+    for word in words:
+        known += len(dictionary.known([word]))
+    score = known/len(words)
+    return score
