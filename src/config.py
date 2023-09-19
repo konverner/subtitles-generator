379827d1eedadf2a6422780e97fd08380d19c6fd
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2023-09-19 23:07:34 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ MODEL_NAMES = {'large': "openai/whisper-large-v2",
                'base': 'openai/whisper-base'}
 
 SAMPLING_RATE = 16000
-CHUNK_SIZE = 4  # a size in seconds of one piece of audio used for one subtitles frame
+CHUNK_SIZE = 2  # a size in seconds of one piece of audio used for one subtitles frame
 BATCH_SIZE = 30  # how many chunks are passed to a model at once
 LIKELIHOOD_TRESHOLD = 0.5  # threshold value for eliminating phrases that contains out-of-dictionary words
 
