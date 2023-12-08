0eeebd2ca6d6d650ca038157be5b83518b1af5fc
konverner <konst.verner@gmail.com>
2023-12-08 11:36:40 +0100

    refacto

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import sys
 import argparse
 import logging
 import warnings
+
 warnings.filterwarnings("ignore")
 
 from pathlib import Path
 from hydra import compose, initialize
 from omegaconf import DictConfig
 
-from src.core import Model
-from src.utils import create_srt, extract_audio
-
+from subtitles_generator.core import Model
+from subtitles_generator.utils import create_srt, extract_audio
 
 logger = logging.getLogger(__name__)
 logging_handler = logging.StreamHandler(sys.stdout)
@@ -20,9 +20,8 @@ logger.addHandler(logging_handler)
 
 
 def parse_args(cfg: DictConfig):
-
     parser = argparse.ArgumentParser(
-      description='It creates subtitles from a video or an audio'
+        description='It creates subtitles from a video or an audio'
     )
 
     parser.add_argument('--model_size', type=str, default='medium',
@@ -47,43 +46,43 @@ def parse_args(cfg: DictConfig):
 
     if lang not in cfg.supported_languages:
         raise ValueError(
-          f"Language {lang} is not supported. Supported languages are {cfg.supported_languages}"
+            f"Language {lang} is not supported. Supported languages are {cfg.supported_languages}"
         )
-    
+
     if not input_file.is_file():
-        raise(OSError(f"Input file {input_file} does not exists"))
+        raise (OSError(f"Input file {input_file} does not exists"))
     if input_file.suffix not in cfg.supported_media_formats.video and \
-       input_file.suffix not in cfg.supported_media_formats.audio:
+            input_file.suffix not in cfg.supported_media_formats.audio:
         raise ValueError(
-          f"""A file must be a video: {cfg.supported_media_formats.video}
+            f"""A file must be a video: {cfg.supported_media_formats.video}
           or an audio {cfg.supported_media_formats.audio}"""
         )
     return input_file, output_file, lang, model_size
 
 
 def app(cfg: DictConfig) -> None:
-  input_file, output_file, lang, model_size = parse_args(cfg)
-  # if an input is video then we extract the audio from it
-  if input_file.suffix in cfg.supported_media_formats.video:
-      logger.info("Extracting audio ...")
-      input_file = extract_audio(input_file)
+    input_file, output_file, lang, model_size = parse_args(cfg)
+    # if an input is video then we extract the audio from it
+    if input_file.suffix in cfg.supported_media_formats.video:
+        logger.info("Extracting audio ...")
+        input_file = extract_audio(input_file)
 
-  model = Model(cfg.model_names[model_size], lang)
-  logger.info("Generating subtitles ...")
-  predicted_texts = model.transcribe(
-      audio_path=input_file,
-      sampling_rate=cfg.processing.sampling_rate,
-      chunk_size=cfg.processing.chunk_size
-  )
+    model = Model(cfg.model_names[model_size], lang)
+    logger.info("Generating subtitles ...")
+    predicted_texts = model.transcribe(
+        audio_path=input_file,
+        sampling_rate=cfg.processing.sampling_rate,
+        chunk_size=cfg.processing.chunk_size
+    )
 
-  logger.info(f"Writing subtitles into {output_file} ...")
-  create_srt(
-    output_file, predicted_texts, cfg.processing.chunk_size, logger=logger
-  )
-  os.remove(input_file)
+    logger.info(f"Writing subtitles into {output_file} ...")
+    create_srt(
+        output_file, predicted_texts, cfg.processing.chunk_size
+    )
+    os.remove(input_file)
 
 
 if __name__ == '__main__':
-  with initialize(version_base=None, config_path="src/conf"):
+    with initialize(version_base=None, config_path="src/subtitles_generator/conf"):
         cfg = compose(config_name="config")
-  app(cfg)
+    app(cfg)
---
+++ setup.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+README_TEXT = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
+
+MAINTAINER = "Konstantin Verner"
+MAINTAINER_EMAIL = "konst.verner@gmail.com"
+REQUIRED_PKGS = [
+    "imageio==2.4.1",
+    "librosa==0.10.1",
+    "hydra-core==1.3.2",
+    "moviepy==0.2.3.5",
+    "numpy==1.23.5",
+    "torch",
+    "transformers==4.35.2"
+]
+
+print(find_packages("src"))
+
+setup(
+    name="subtitles-generator",
+    version="0.0.1",
+    description="Few-Shot Voice Cloning",
+    long_description=README_TEXT,
+    long_description_content_type="text/markdown",
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url="",
+    download_url="",
+    license="MIT",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    include_package_data=True,
+    package_data={"": ["*.json"]},
+    install_requires=REQUIRED_PKGS,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="subtitles, nlp, generation",
+    zip_safe=False,  # Required for mypy to find the py.typed file
+)
